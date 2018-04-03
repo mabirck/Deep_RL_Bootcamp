d1bcbb111237c582d37c10bbea44ad0365526b00
Marco Birck <mafbirck@gmail.com>
2018-04-03 20:25:42 -0300

    TRPO

--- lab4/trpo.py
+++ lab4/trpo.py
@@ -164,6 +164,10 @@ def trpo(env, env_maker, policy, baseline, n_envs=mp.cpu_count(), last_iter=-1, 
                     """
                     surr_loss = Variable(np.array(0.))
                     "*** YOUR CODE HERE ***"
+
+                    surr_loss = new_dists.likelihood_ratio(old_dists, all_acts)
+                    surr_loss = -1 * F.mean(surr_loss * all_advs)
+
                     return surr_loss
 
                 def compute_kl(old_dists, new_dists):
@@ -174,6 +178,7 @@ def trpo(env, env_maker, policy, baseline, n_envs=mp.cpu_count(), last_iter=-1, 
                     """
                     kl = Variable(np.array(0.))
                     "*** YOUR CODE HERE ***"
+                    kl = F.mean(old_dists.kl_div(new_dists))
                     return kl
 
                 test_once(compute_surr_loss)
